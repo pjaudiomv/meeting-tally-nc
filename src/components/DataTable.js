826fd0ea07bf4d47df9cb12e30c042edf6a53918
Patrick Joyce <pjaudiomv@gmail.com>
2022-12-04 09:56:13 -0500

    north

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -331,7 +331,7 @@ export default function DataTable({ meetings, serviceBodies }) {
               </IconButton>
             </TableCell>
             <TableCell style={{ fontWeight: 600, color: "#fff" }}>
-              Carolina Region
+              North Carolina Region
             </TableCell>
             <TableCell
               align="center"
